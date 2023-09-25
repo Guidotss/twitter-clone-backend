e4954a77badac4cd2f0284821c82935aa4a604c5
Guidotss <guidoolguin@hotmail.com>
2023-09-25 10:07:14 -0300

    Creando ApplicationDbContext en proyecto DataAccess

---
+++ DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Models; 
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Reflection;
+
+namespace DataAccess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<User> User { get; set; } = default!;
+        public DbSet<Tweet> Tweet { get; set; } = default!;
+        public DbSet<Follow> Follow { get; set; } = default!;
+        public DbSet<Retweet> Retweet { get; set; } = default!;
+        public DbSet<Like> Like { get; set; } = default!;
+        public DbSet<Comment> Comment { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+             base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly()); 
+        }
+    }
+}
