088673d15262ccae1c3fdd82ea3c3d5bc65f9366
Guidotss <guidoolguin@hotmail.com>
2023-09-26 11:33:07 -0300

    Creando e implementando interfaz de repositorio

---
+++ DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<T> GetAsync(Guid id); 
+        Task<IEnumerable<T>> GetAllAsync(
+            Expression<Func<T,bool>>filter = null,
+            Func<IQueryable<T>,IOrderedQueryable<T>> orderBy = null,
+            string includeProperties = "",
+            bool isTracking = true
+        );
+        Task<T> GetFirst(
+            Expression<Func<T, bool>> filter = null,
+            string includeProperties = "",
+            bool isTracking = true
+        );
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entity); 
+        void RemoveAsync(Guid id);
+        void RemoveRangeAsync(IEnumerable<T> entity); 
+    }
+}
---
+++ DataAccess/Repository/Repository.cs
@@ -0,0 +1,91 @@
+﻿using DataAccess.Data;
+using DataAccess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _db;
+        internal DbSet<T> dbSet; 
+
+        public Repository( ApplicationDbContext db)
+        {
+            _db = db;
+            dbSet = _db.Set<T>(); 
+        }
+        public async Task AddAsync(T entity) => await dbSet.AddAsync(entity);
+        public async Task AddRangeAsync(IEnumerable<T> entity) => await dbSet.AddRangeAsync(entity);
+        public async Task<IEnumerable<T>> GetAllAsync(Expression<Func<T, bool>> filter = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string includeProperties = "", bool isTracking = true)
+        {
+            IQueryable<T> query = dbSet;
+
+            if(filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            if(orderBy != null)
+            {
+                query = orderBy(query); 
+            }
+
+            if(includeProperties != null)
+            {
+                foreach(var includeProp in includeProperties.Split(new char[] {','}, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProp);
+                }
+            }
+
+            if(!isTracking)
+            {
+                query = query.AsNoTracking();
+            }
+            
+            return await query.ToListAsync();
+        }
+
+        public async Task<T> GetAsync(Guid id) => await dbSet.FindAsync(id);
+        public Task<T> GetFirst(Expression<Func<T, bool>> filter = null, string includeProperties = "", bool isTracking = true)
+        {
+            IQueryable<T> query = dbSet;
+
+            if(filter != null)
+            {
+                query = query.Where(filter); 
+            }
+
+            if(includeProperties != null)
+            {
+                foreach(var includeProps in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProps); 
+                }
+            }
+
+            if(!isTracking)
+            {
+                query = query.AsNoTracking(); 
+            }
+
+            return query.FirstOrDefaultAsync(); 
+        }
+        public void RemoveAsync(Guid id)
+        {
+            var entity = dbSet.Find(id);
+            if(entity != null)
+            {
+                dbSet.Remove(entity);
+            }
+        }
+        public void RemoveRangeAsync(IEnumerable<T> entity) => dbSet.RemoveRange(entity);
+        
+    }
+}
