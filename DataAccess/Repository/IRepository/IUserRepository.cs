4c7a51571134dc66ae9f0581cf83e0e436f37c09
Guidotss <guidoolguin@hotmail.com>
2023-09-26 12:06:29 -0300

    Implementando interfaz de Repositorio de usuario

---
+++ DataAccess/Repository/IRepository/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models; 
+
+namespace DataAccess.Repository.IRepository
+{
+    public interface IUserRepository : IRepository<User>
+    {
+        public void Update(User user);
+        public string HashPassword(string password);
+        public bool VerifyPassword(User user,string password);
+        public Task<User> GetUserByEmail(string email);   
+    }
+}
---
+++ DataAccess/Repository/UserRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Data;
+using DataAccess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using Models; 
+
+namespace DataAccess.Repository
+{
+    public class UserRepository : Repository<User>, IUserRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public UserRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db; 
+        }
+
+        public async Task<User> GetUserByEmail(string email) => await _db.User.FirstOrDefaultAsync(u => u.Email == email);
+        public string HashPassword(string password) => BCrypt.Net.BCrypt.HashPassword(password);
+        public bool VerifyPassword(User user, string password) => BCrypt.Net.BCrypt.Verify(password, user.Password);
+
+
+        public async void Update(User user)
+        {
+            var userFromDb = _db.User.FirstOrDefault(u => u.Id == user.Id);
+            if(userFromDb != null)
+            {
+                userFromDb.Name = user.Name;
+                userFromDb.Email = user.Email;
+                userFromDb.Password = user.Password;
+                userFromDb.ImageUrl = user.ImageUrl;
+                userFromDb.Bio = user.Bio;
+                await _db.SaveChangesAsync();
+            }
+        }
+
+    }
+}; 
